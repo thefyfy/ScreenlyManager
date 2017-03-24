9ebf33b8ee90cb090928ef8539ccd01bec819c0f
thefyfy <jeremy.filhoulaud@gmail.com>
2017-03-24 10:31:32 +0100

    Now can add new device

--- AddOrChangeDevicePage.xaml.cs
+++ AddOrChangeDevicePage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,9 +26,70 @@ namespace ScreenlyManager
     /// </summary>
     public sealed partial class AddOrChangeDevicePage : Page
     {
+        private List<Device> Devices;
+        private const string DB_FILE = "db.json";
+
         public AddOrChangeDevicePage()
         {
+            this.Devices = new List<Device>();
+
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.Devices = e.Parameter as List<Device>;
+        }
+
+        private async void ButtonSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if(!this.TextBoxName.Text.Equals(string.Empty) && !this.TextBoxIp.Text.Equals(string.Empty) && !this.TextBoxPort.Equals(string.Empty))
+            {
+                Device newDevice = new Device();
+                newDevice.Name = this.TextBoxName.Text;
+                newDevice.Location = this.TextBoxLocation.Text;
+                newDevice.IpAddress = this.TextBoxIp.Text;
+                newDevice.Port = this.TextBoxPort.Text;
+                this.Devices.Add(newDevice);
+
+                var dbContent = JsonConvert.SerializeObject(this.Devices);
+
+                var pathDbFile = ApplicationData.Current.LocalFolder.Path + Path.DirectorySeparatorChar + DB_FILE;
+                StorageFile file = await StorageFile.GetFileFromPathAsync(pathDbFile);
+
+                if (file != null)
+                {
+                    CachedFileManager.DeferUpdates(file);
+                    await FileIO.WriteTextAsync(file, dbContent);
+                    Windows.Storage.Provider.FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+                    var dialog = new MessageDialog($"The new device has been added");
+                    if (status != Windows.Storage.Provider.FileUpdateStatus.Complete)
+                    {
+                        dialog.Content = $"Cannot save configuration.";
+                        dialog.Title = "Error";
+                    }
+                    dialog.Commands.Add(new UICommand("Ok") { Id = 0 });
+                    dialog.DefaultCommandIndex = 0;
+                    var result = await dialog.ShowAsync();
+
+                    this.Frame.Navigate(typeof(MainPage), null);
+                }
+            }
+            else
+            {
+                var dialogError = new MessageDialog($"Oops... You have to fill at least this fields : \"Name\", \"IP Address\" and \"Port\"");
+                dialogError.Commands.Add(new UICommand("Ok") { Id = 0 });
+                dialogError.DefaultCommandIndex = 0;
+                await dialogError.ShowAsync();
+
+                this.TextBoxName.Focus(FocusState.Programmatic);
+                this.TextBoxName.SelectAll();
+            }
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
         }
     }
 }
